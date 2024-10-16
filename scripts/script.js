5ceabd79a9d43422b9e0c4dd3928f375b7848b11
vit che <privsl408@gmail.com>
2024-10-16 03:03:44 +0300

    "sxxxxxx"

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,64 @@
-const themeButtons = document.querySelectorAll('.header-modified__menu-button');
+const themeButtons = document.querySelectorAll(".header-modified__menu-button");
 
 themeButtons.forEach((button) => {
-  button.addEventListener('click', () => {
+  button.addEventListener("click", () => {
     themeButtons.forEach((btn) => {
-      btn.classList.remove('header-modified__menu-button_active');
-      btn.removeAttribute('disabled');
+      btn.classList.remove("header-modified__menu-button_active");
+      btn.removeAttribute("disabled");
     });
     if (
-      [...button.classList].includes('header-modified__menu-button_type_light')
+      [...button.classList].includes("header-modified__menu-button_type_light")
     ) {
-      changeTheme('light');
+      changeTheme("light");
     } else if (
-      [...button.classList].includes('header-modified__menu-button_type_dark')
+      [...button.classList].includes("header-modified__menu-button_type_dark")
     ) {
-      changeTheme('dark');
+      changeTheme("dark");
     } else {
-      changeTheme('auto');
+      changeTheme("auto");
     }
-    button.classList.add('header-modified__menu-button_active');
-    button.setAttribute('disabled', true);
+    button.classList.add("header-modified__menu-button_active");
+    button.setAttribute("disabled", true);
   });
 });
 
 function changeTheme(theme) {
-  document.body.className = 'page-modified';
+  const currentHour = new Date().getHours();
+  console.log(currentHour)
+  if (theme === "auto") {
+    if (currentHour > 9 && currentHour < 21) {
+      document.body.className = "page-modified";
+      document.body.classList.add(`theme_light`);
+      localStorage.setItem("theme", theme);
+      return
+    } else {
+      document.body.className = "page-modified";
+      document.body.classList.add(`theme_dark`);
+      localStorage.setItem("theme", theme);
+      return
+    } 
+  }
+  document.body.className = "page-modified";
   document.body.classList.add(`theme_${theme}`);
-  localStorage.setItem('theme', theme);
+  localStorage.setItem("theme", theme);
 }
 
 function initTheme() {
-  const theme = localStorage.getItem('theme');
+  const theme = localStorage.getItem("theme");
   if (theme) {
-    changeTheme(theme);
+    changeTheme(theme) 
     themeButtons.forEach((btn) => {
-      btn.classList.remove('header-modified__menu-button_active');
-      btn.removeAttribute('disabled');
+      btn.classList.remove("header-modified__menu-button_active");
+      btn.removeAttribute("disabled");
     });
     document
       .querySelector(`.header-modified__menu-button_type_${theme}`)
-      .classList.add('header-modified__menu-button_active');
+      .classList.add("header-modified__menu-button_active");
     document
       .querySelector(`.header-modified__menu-button_type_${theme}`)
-      .setAttribute('disabled', true);
+      .setAttribute("disabled", true);
   }
 }
 
 initTheme();
+
